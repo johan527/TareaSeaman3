a623dd0e195de098a5c40bf63a4f69dbf5424e08
johan527 <104233117+johan527@users.noreply.github.com>
2023-01-31 23:15:31 -0600

    Avance martes

---
+++ tareaSemana3/src/main/java/com/tareaSemana3/demo/TareaSemana3Application.java
@@ -0,0 +1,13 @@
+package com.tareaSemana3.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaSemana3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareaSemana3Application.class, args);
+	}
+
+}
---
+++ tareaSemana3/src/test/java/com/tareaSemana3/demo/TareaSemana3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tareaSemana3.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareaSemana3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
